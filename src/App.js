7877aaa7eb06d0c9e1fe582741d007127ac26901
Mehreteab <mirehabtecoen@gmail.com>
2021-06-10 14:34:47 -0700

    Task events defined

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import {useState} from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 function App() { 
+
+  const [tasks, setTasks] = useState(
+    [
+        {
+            "id": 1,
+            "text": "Doctors Appointment",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": true
+          },
+          {
+            "id": 2,
+            "text": "Meeting at School",
+            "day": "Feb 6th at 1:30pm",
+            "reminder": true
+          }
+    ]
+)
+
+ //Delete Task
+  const deleteTask = (id) => {
+     console.log('delete', id)
+     setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+ //Toggle remainder
+  const toggleRemainder = (id) => {
+    console.log('Toggled', id)
+    setTasks(tasks.map((task) => 
+      task.id === id ? { ...task, reminder: !task.reminder} : task
+    ))
+  }
 
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks}
+       onDelete={deleteTask} 
+       onToggle={toggleRemainder} /> : 'No tasks available'}
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import PropTypes from 'prop-types';
+import Button from './Button'
 const Header = ({title}) => {
+
+    const onClick = () => {
+        console.log('Clicked')
+    }
+    
     return (
-        <div>
-            <h1 /* style={headingStyle} */>{title}</h1>
+        <div className="header">
+            <h1 /* style={headingStyle} */>{title}</h1> 
+            <Button text='Add' color='red' onClick={onClick} />          
         </div>
     )
 }
